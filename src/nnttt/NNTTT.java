0553abb4f823724fb9468207612bfd8c5e0ef0f5
Lindqvist Oliver <olindqvi@da3-cs-dk110-10.pc.helsinki.fi>
2017-10-31 18:41:13 +0200

    Alustus

---
+++ src/nnttt/NNTTT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nnttt;
+
+/**
+ *
+ * @author olindqvi
+ */
+public class NNTTT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
